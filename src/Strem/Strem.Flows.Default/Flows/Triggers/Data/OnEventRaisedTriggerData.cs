058e9599a0ab2e7c0daffcf9ccf323507246faa6
LP <grofit1@yahoo.com>
2022-08-04 19:47:27 +0100

    Added event raising tasks

---
+++ src/Strem/Strem.Flows.Default/Flows/Triggers/Data/OnEventRaisedTriggerData.cs
@@ -0,0 +1,11 @@
+﻿using Strem.Core.Flows.Triggers;
+
+namespace Strem.Flows.Default.Flows.Triggers.Data;
+
+public class OnEventRaisedTriggerData : IFlowTriggerData
+{
+    public string Code => OnEventRaisedTrigger.TriggerCode;
+    public string Version { get; set; } = OnEventRaisedTrigger.TriggerVersion;
+    
+    public string EventName { get; set; }
+}
---
+++ src/Strem/Strem.Flows.Default/Flows/Triggers/OnEventRaisedTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Reactive.Linq;
+using Microsoft.Extensions.Logging;
+using Strem.Core.Events.Bus;
+using Strem.Core.Extensions;
+using Strem.Core.Flows.Processors;
+using Strem.Core.Flows.Triggers;
+using Strem.Core.State;
+using Strem.Core.Variables;
+using Strem.Flows.Default.Events;
+using Strem.Flows.Default.Flows.Triggers.Data;
+
+namespace Strem.Flows.Default.Flows.Triggers;
+
+public class OnEventRaisedTrigger : FlowTrigger<OnEventRaisedTriggerData>
+{
+    public static readonly string TriggerCode = "on-event-raised";
+    public static readonly string TriggerVersion = "1.0.0";
+    public override string Code => TriggerCode;
+    public override string Version => TriggerVersion;
+
+    public override string Name => "On Event Raised";
+    public override string Description => "Triggers when the matching event is raised";
+
+    public OnEventRaisedTrigger(ILogger<IFlowTrigger> logger, IFlowStringProcessor flowStringProcessor, IAppState appState, IEventBus eventBus) 
+        : base(logger, flowStringProcessor, appState, eventBus)
+    {
+    }
+
+    public override IObservable<IVariables> Execute(OnEventRaisedTriggerData data)
+    {
+        return EventBus.Receive<UserDataEvent>()
+            .Where(x => x.EventName == data.EventName)
+            .Select(x =>
+            {
+                var newVariables = new Variables();
+                newVariables.Set("event-data", x.Data);
+                return newVariables;
+            });
+    }
+}
