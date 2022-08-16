83de349c7ce43c81d30b6c1d849625314816b4ff
LP <grofit1@yahoo.com>
2022-08-16 20:53:39 +0100

    Messing with build again

---
+++ src/Strem.Build/Orchestration/BuildAndTestTask.cs
@@ -0,0 +1,13 @@
+﻿using Cake.Frosting;
+using Strem.Build.Tasks;
+
+namespace Strem.Build.Orchestration;
+
+[TaskName("build-and-test")]
+[IsDependentOn(typeof(CleanDirectoriesTask))]
+[IsDependentOn(typeof(BuildSolutionTask))]
+[IsDependentOn(typeof(RunUnitTestsTask))]
+public class BuildAndTestTask : FrostingTask<BuildContext>
+{
+    
+}
---
+++ src/Strem.Build/Orchestration/PublishTask.cs
@@ -0,0 +1,11 @@
+﻿using Cake.Frosting;
+using Strem.Build.Tasks;
+
+namespace Strem.Build.Orchestration;
+
+[TaskName("publish")]
+[IsDependentOn(typeof(PackageAppTask))]
+public class PublishTask : FrostingTask<BuildContext>
+{
+    
+}
