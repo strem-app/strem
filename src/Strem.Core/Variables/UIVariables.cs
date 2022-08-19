87853d8b75631d1aa140905a00c4aec1d64dc0eb
LP <grofit1@yahoo.com>
2022-08-19 16:32:04 +0100

    Added ability to zoom UI
    It has some quirky hacks to make it function but it should work for all intents and purposes.

--- src/Strem.Core/Variables/UIVariables.cs
+++ src/Strem.Core/Variables/UIVariables.cs
@@ -5,4 +5,5 @@ public class UIVariables
     public static readonly VariableEntry ShowHelpersVariable = new VariableEntry("show-helpers", "ui");
     public static readonly VariableEntry BackupFrequencyInDays = new VariableEntry("backup.frequency", "ui");
     public static readonly VariableEntry LastBackupDate = new VariableEntry("backup.last-run", "ui");
+    public static readonly VariableEntry ZoomVariable = new VariableEntry("zoom", "ui");
 }
---
+++ src/Strem/wwwroot/js/interop-methods.js
@@ -0,0 +1,15 @@
+﻿function setZoom(newSize)
+{
+    document.body.style.zoom = `${newSize}%`;
+    let scaleAlteration = ((100-newSize)/100);
+    if(scaleAlteration >= 0) { scaleAlteration *= 1.5; }
+    if(scaleAlteration < 0) { scaleAlteration /= 1.5; }
+
+
+    console.log("alteration", scaleAlteration);
+    const scalingFactor = 1+scaleAlteration;
+    const base = 100;
+    const newVhScaling = (100 * scalingFactor) + "vh";
+    console.log("new scaling", newVhScaling);
+    document.querySelectorAll(".is-fullheight").forEach(x => x.style.minHeight = newVhScaling);
+}
