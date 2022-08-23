fc85406a7a8864fbaf10f25c6479c7c82ec259f7
LP <grofit1@yahoo.com>
2022-08-23 22:46:56 +0100

    Moving to generic drag and drop components

---
+++ src/Strem.Core/Components/Elements/Drag/DragController.cs
@@ -0,0 +1,38 @@
+﻿using System.Reactive.Subjects;
+
+namespace Strem.Core.Components.Elements.Drag;
+
+public class DragController
+{
+    public object? DraggingObject { get; private set; }
+    public object? LastEnteredObject { get; private set; }
+    
+    private readonly Subject<DropData> _droppedItemSubject = new();
+    public IObservable<DropData> OnDroppedItem => _droppedItemSubject;
+
+    public void OnDropOverElement(object dropTarget)
+    {
+        LastEnteredObject=null;
+        if (DraggingObject == null) return;
+        if (DraggingObject == dropTarget) return;
+
+        var dropData = new DropData(DraggingObject, "", dropTarget);
+        _droppedItemSubject.OnNext(dropData);
+        DraggingObject=null;
+    }
+
+    public void OnDragStart(object draggingObject)
+    { DraggingObject = draggingObject; }
+
+    public void OnDragEnterElement(object enteredItem)
+    {
+        if (DraggingObject == null) { return; }
+        LastEnteredObject = enteredItem;
+    }
+
+    public void  OnDragEnd()
+    {
+        DraggingObject = null;        
+        LastEnteredObject = null;
+    }
+}
---
+++ src/Strem.Core/Components/Elements/Drag/DropData.cs
@@ -0,0 +1,15 @@
+﻿namespace Strem.Core.Components.Elements.Drag;
+
+public class DropData
+{
+    public object DraggedItem { get; } 
+    public object? DroppedOn { get; }
+    public string Context { get; }
+
+    public DropData(object draggedItem, string context, object? droppedOn = null)
+    {
+        DraggedItem = draggedItem;
+        DroppedOn = droppedOn;
+        Context = context;
+    }
+}
