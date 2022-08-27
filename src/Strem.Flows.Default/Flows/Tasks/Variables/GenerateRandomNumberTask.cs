eb478bd11846b27a3842c1a2262d94d8c097e18b
LP <grofit1@yahoo.com>
2022-08-27 21:49:16 +0100

    Added random number/string generation tasks

---
+++ src/Strem.Flows.Default/Flows/Tasks/Variables/GenerateRandomNumberTask.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Logging;
+using Strem.Core.Events.Bus;
+using Strem.Core.Extensions;
+using Strem.Core.Flows.Executors;
+using Strem.Core.Flows.Processors;
+using Strem.Core.Flows.Tasks;
+using Strem.Core.State;
+using Strem.Core.Types;
+using Strem.Core.Utils;
+using Strem.Core.Variables;
+
+namespace Strem.Flows.Default.Flows.Tasks.Variables;
+
+public class GenerateRandomNumberTask : FlowTask<GenerateRandomNumberTaskData>
+{
+    public override string Code => GenerateRandomNumberTaskData.TaskCode;
+    public override string Version => GenerateRandomNumberTaskData.TaskVersion;
+    
+    public override string Name => "Generate A Random Number";
+    public override string Category => "Variables";
+    public override string Description => "Generates a random number between the given range";
+    
+    public IRandomizer Randomizer { get; }
+
+    public GenerateRandomNumberTask(ILogger<FlowTask<GenerateRandomNumberTaskData>> logger, IFlowStringProcessor flowStringProcessor, IAppState appState, IEventBus eventBus, IRandomizer randomizer) : base(logger, flowStringProcessor, appState, eventBus)
+    {
+        Randomizer = randomizer;
+    }
+
+    public override bool CanExecute() => true;
+
+    public override async Task<ExecutionResult> Execute(GenerateRandomNumberTaskData data, IVariables flowVars)
+    {
+        var processedName = FlowStringProcessor.Process(data.Name, flowVars);
+        var processedContext = FlowStringProcessor.Process(data.Context, flowVars);
+
+        if (FlowStringProcessor.TryProcessInt(data.MinimumNumber, flowVars, out var processedMinValue))
+        {
+            Logger.Warning($"Couldnt process minimum value {data.MinimumNumber}");
+            processedMinValue = 0;
+        }
+        
+        if (FlowStringProcessor.TryProcessInt(data.MaximumNumber, flowVars, out var processedMaxValue))
+        {
+            Logger.Warning($"Couldnt process maximum value {data.MaximumNumber}");
+            processedMaxValue = 100;
+        }
+
+        var randomValue = Randomizer.Random(processedMinValue, processedMaxValue);
+        AppState.SetVariable(flowVars, data.ScopeType, processedName, processedContext, randomValue.ToString());
+        return ExecutionResult.Success();
+    }
+}
---
+++ src/Strem.Flows.Default/Flows/Tasks/Variables/GenerateRandomStringTask.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+using Strem.Core.Events.Bus;
+using Strem.Core.Extensions;
+using Strem.Core.Flows.Executors;
+using Strem.Core.Flows.Processors;
+using Strem.Core.Flows.Tasks;
+using Strem.Core.State;
+using Strem.Core.Utils;
+using Strem.Core.Variables;
+
+namespace Strem.Flows.Default.Flows.Tasks.Variables;
+
+public class GenerateRandomStringTask : FlowTask<GenerateRandomStringTaskData>
+{
+    public override string Code => GenerateRandomStringTaskData.TaskCode;
+    public override string Version => GenerateRandomStringTaskData.TaskVersion;
+    
+    public override string Name => "Generate A Random String";
+    public override string Category => "Variables";
+    public override string Description => "Generates a random string of given length";
+    
+    public IRandomizer Randomizer { get; }
+
+    public GenerateRandomStringTask(ILogger<FlowTask<GenerateRandomStringTaskData>> logger, IFlowStringProcessor flowStringProcessor, IAppState appState, IEventBus eventBus, IRandomizer randomizer) : base(logger, flowStringProcessor, appState, eventBus)
+    {
+        Randomizer = randomizer;
+    }
+
+    public override bool CanExecute() => true;
+
+    public override async Task<ExecutionResult> Execute(GenerateRandomStringTaskData data, IVariables flowVars)
+    {
+        var processedName = FlowStringProcessor.Process(data.Name, flowVars);
+        var processedContext = FlowStringProcessor.Process(data.Context, flowVars);
+
+        if (FlowStringProcessor.TryProcessInt(data.StringLength, flowVars, out var processedLength))
+        {
+            Logger.Warning($"Couldnt process minimum value {data.StringLength}");
+            processedLength = 10;
+        }
+        
+        var randomValue = Randomizer.RandomString(processedLength);
+        AppState.SetVariable(flowVars, data.ScopeType, processedName, processedContext, randomValue);
+        return ExecutionResult.Success();
+    }
+}
