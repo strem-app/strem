c68c9d2e54a072c3ca8cb4e0e0a9f0bc8684ea58
LP <grofit1@yahoo.com>
2022-09-02 13:24:09 +0100

    Added basic validation to trigger

---
+++ src/Strem.Core/Components/Elements/ValidatedInputBase.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq.Expressions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Strem.Core.Components.Elements;
+
+public abstract class ValidatedInputBase<T> : InputBase<T>
+{
+    [CascadingParameter]
+    protected EditContext? EditContext { get; set; }
+    protected FieldIdentifier? _fieldIdentifier { get; set; }
+    
+    public bool? IsValid
+    {
+        get
+        {
+            if (_fieldIdentifier is not { }){ return null; }
+            return !EditContext.GetValidationMessages(_fieldIdentifier.Value).Any();
+        }
+    }
+    
+    protected override async Task OnInitializedAsync()
+    {
+        if (EditContext != null)
+        { _fieldIdentifier = FieldIdentifier.Create(ValueExpression); }
+        await base.OnInitializedAsync();
+    }
+    
+    public void OnValueChanged(T value)
+    {
+        ValueChanged.InvokeAsync(value);
+        if(_fieldIdentifier != null)
+        { EditContext?.NotifyFieldChanged(_fieldIdentifier.Value); }
+    }
+}
