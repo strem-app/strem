a5d62d447fbff799cfb303131fd0eb5c7c0f289b
LP <grofit1@yahoo.com>
2022-09-16 14:14:14 +0100

    Updates portals
    Added chunky click animations on viewer to make it clear a button has been pressed.

--- src/Strem/wwwroot/js/interop-methods.js
+++ src/Strem/wwwroot/js/interop-methods.js
@@ -28,23 +28,22 @@ function showNotification(text, type, duration)
     bulmaToast.toast({ message: text, type: type, duration: duration });
 }
 
-const animateCSS = (element, animation, prefix = 'animate__') =>
+function animateElement(element, animation, prefix = 'animate__') {
     // We create a Promise and return it
     new Promise((resolve, reject) => {
         const animationName = `${prefix}${animation}`;
-        const node = document.querySelector(element);
-
-        node.classList.add(`${prefix}animated`, animationName);
-
+        element.classList.add(`${prefix}animated`, animationName);
+        
         // When the animation ends, we clean the classes and resolve the Promise
         function handleAnimationEnd(event) {
             event.stopPropagation();
-            node.classList.remove(`${prefix}animated`, animationName);
+            element.classList.remove(`${prefix}animated`, animationName);
             resolve('Animation ended');
         }
-
-        node.addEventListener('animationend', handleAnimationEnd, {once: true});
+        
+        element.addEventListener('animationend', handleAnimationEnd, {once: true});
     });
+}
 
 bulmaToast.setDefaults({
     duration: 2000,
