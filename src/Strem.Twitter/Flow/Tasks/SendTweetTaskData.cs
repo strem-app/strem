712ce4c5fff7df9d2f6bb5d2e0086e81d99c7e4d
LP <grofit1@yahoo.com>
2022-10-01 22:58:48 +0100

    Added basic first task
    Needs implementation

---
+++ src/Strem.Twitter/Flow/Tasks/SendTweetTaskData.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Strem.Flows.Data.Tasks;
+
+namespace Strem.Twitter.Flow.Tasks;
+
+public class SendTweetTaskData : IFlowTaskData
+{
+    public static readonly string TaskCode = "send-tweet-task-data";
+    public static readonly string TaskVersion = "1.0.0";
+    
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Code => TaskCode;
+    public string Version { get; set; } = TaskVersion;
+    
+    [Required]
+    public string TweetContent { get; set; }
+}
---
+++ src/Strem.Twitter/Flow/Tasks/SetStreamTitleTask.cs
@@ -0,0 +1,32 @@
+﻿using Strem.Core.Events.Bus;
+using Strem.Core.State;
+using Strem.Core.Variables;
+using Strem.Flows.Data.Tasks;
+using Strem.Flows.Executors;
+using Strem.Flows.Processors;
+using Strem.Twitter.Extensions;
+using Strem.Twitter.Types;
+
+namespace Strem.Twitter.Flow.Tasks;
+
+public class SendTweetTask : FlowTask<SendTweetTaskData>
+{
+    public override string Code => SendTweetTaskData.TaskCode;
+    public override string Version => SendTweetTaskData.TaskVersion;
+    
+    public override string Name => "Send A Tweet";
+    public override string Category => "Twitter";
+    public override string Description => "Sends a tweet";
+    
+    public SendTweetTask(ILogger<FlowTask<SendTweetTaskData>> logger, IFlowStringProcessor flowStringProcessor, IAppState appState, IEventBus eventBus) : base(logger, flowStringProcessor, appState, eventBus)
+    {
+    }
+
+    public override bool CanExecute() => AppState.HasTwitterOAuth() && AppState.HasTwitterScope(Scopes.WriteTweet);
+
+    public override async Task<ExecutionResult> Execute(SendTweetTaskData data, IVariables flowVars)
+    {
+        var processedTweet = FlowStringProcessor.Process(data.TweetContent, flowVars);
+        return ExecutionResult.Success();
+    }
+}
