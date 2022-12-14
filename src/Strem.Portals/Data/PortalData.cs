f75dc24e9af15cd114c7a9a38d05de9e8e4ab2ec
LP <grofit1@yahoo.com>
2022-12-14 16:49:18 +0000

    Fixed the issue with not saving buttons
    This should now save them correctly on modal closing, there is still and issue though around the button changed event values not propogating.

--- src/Strem.Portals/Data/PortalData.cs
+++ src/Strem.Portals/Data/PortalData.cs
@@ -1,11 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using Strem.Portals.Types;
 
 namespace Strem.Portals.Data;
 
 public class PortalData
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Version { get; set; } = "1.0.0";
     
     [Required]
     public string Name { get; set; }
@@ -17,7 +17,6 @@ public class PortalData
     [Range(1, 100, ErrorMessage = "{0} must be between 1 and 100 in size")]
     public int ElementGridSize { get; set; } = 20;
     
-    public ButtonSize ElementSize { get; set; }
     public List<GridElementData> Elements { get; set; } = new();
 
     public PortalData(Guid id, string name)
