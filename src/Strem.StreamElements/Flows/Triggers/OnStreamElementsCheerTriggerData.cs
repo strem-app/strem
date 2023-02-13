0df6635f00adfa84e9809e467b1fe1df93da6a36
LP <grofit1@yahoo.com>
2023-02-13 16:09:07 +0000

    Adding cheer trigger
    This kinda already exists in the twitch one, but not sure if there is some reason to use this instead so will include it anyway.

---
+++ src/Strem.StreamElements/Flows/Triggers/OnStreamElementsCheerTriggerData.cs
@@ -0,0 +1,13 @@
+﻿using Strem.Flows.Data.Triggers;
+
+namespace Strem.StreamElements.Flows.Triggers;
+
+public class OnStreamElementsCheerTriggerData : IFlowTriggerData
+{
+    public static readonly string TriggerCode = "on-stream-elements-cheer";
+    public static readonly string TriggerVersion = "1.0.0";
+
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Code => TriggerCode;
+    public string Version { get; set; } = TriggerVersion;
+}
