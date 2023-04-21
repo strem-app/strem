20d269d7977688a0daa4343e92e4858e43fe5ac9
LP <grofit1@yahoo.com>
2023-04-21 09:16:51 +0100

    Adding notion of exclusive execution handler
    This covers the part of the puzzle where flows can hold locks for a period and delay other tasks from execution.

---
+++ src/Strem.Core/Services/Execution/ExclusiveExecutionHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Reactive;
+
+namespace Strem.Core.Services.Execution;
+
+public class ExclusiveExecutionHandler : IExclusiveExecutionHandler
+{
+    public Dictionary<string, bool> ExclusivityGroupLocks { get; } = new();
+    public object lockHandle = new();
+
+    public async Task<bool> RequestLockFor(string group, CancellationToken cancellationToken = default)
+    {
+        if (!ExclusivityGroupLocks.ContainsKey(group))
+        {
+            lock (lockHandle) 
+            { ExclusivityGroupLocks.Add(group, true); }
+            return true;
+        }
+
+        var isLocked = ExclusivityGroupLocks[group];
+        if (isLocked)
+        {
+            while (ExclusivityGroupLocks[group])
+            {
+                await Task.Delay(250, cancellationToken);
+                
+                if (cancellationToken.IsCancellationRequested)
+                { return false; }
+            }
+        }
+        
+        lock (lockHandle)
+        { ExclusivityGroupLocks[group] = true; }
+
+        return true;
+    }
+
+    public void FreeLockFor(string group)
+    {
+        if(!ExclusivityGroupLocks.ContainsKey(group))
+        { return; }
+        
+        var isLocked = ExclusivityGroupLocks[group];
+        if (!isLocked)
+        { return; }
+
+        lock (lockHandle)
+        { ExclusivityGroupLocks[group] = false; }
+    }
+}
---
+++ src/Strem.Core/Services/Execution/IExclusiveExecutionHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace Strem.Core.Services.Execution;
+
+public interface IExclusiveExecutionHandler
+{
+    Task<bool> RequestLockFor(string group, CancellationToken cancellationToken = default);
+    void FreeLockFor(string group);
+}
